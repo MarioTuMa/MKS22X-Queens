eed851dd1fe8912a3780e12af3a66ed6102e815e
Marioisdabest <mtutuncumacias1@gmail.com>
2019-02-08 11:02:24 -0500

    Add files via upload

--- QueenBoard.java
+++ QueenBoard.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class QueenBoard{
 
   private int[][]board;
-
+  private int queen_counter = 0;
   public static boolean attacks(int row1, int col1, int row2, int col2){
     return (row1 == row2) || (col1 == col2) || (Math.abs(col1 - col2) == Math.abs(row1 - row2));
   }
@@ -16,11 +16,29 @@ public class QueenBoard{
   }
   private boolean addQueen(int row, int col){
     if(board[row][col]==0){
+      queen_counter++;
       board[row][col] = -1;
       for(int i = row + 1; i < board.length; i ++){
         for(int j = 0; j < board.length; j ++){
           if(attacks(i,j,row,col)){
             board[i][j] += 1;
+          }
+        }
+      }
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+  private boolean removeQueen(int row, int col){
+    if(board[row][col]==-1){
+      queen_counter--;
+      board[row][col] = 0;
+      for(int i = row + 1; i < board.length; i ++){
+        for(int j = 0; j < board.length; j ++){
+          if(attacks(i,j,row,col)){
+            board[i][j] -= 1;
           }
         }
       }
@@ -44,7 +62,23 @@ public class QueenBoard{
 
 
   public boolean solve(){
-    return true;
+    if(queen_counter == board.length){
+      System.out.println(board.toString());
+      return true;
+    }
+    else{
+
+      for(int i = 0; i < board.length; i ++){
+        if(board[queen_counter][i] == 0){
+          addQueen(queen_counter,i);
+          if(solve()){
+            return true;
+          }
+          removeQueen(queen_counter,i);
+        }
+      }
+      return false;
+    }
   }
 
   public int countSolutions(){
@@ -52,9 +86,13 @@ public class QueenBoard{
   }
   public static void main(String args[]){
     System.out.println("hi");
-    QueenBoard qb = new QueenBoard(5);
+    QueenBoard qb = new QueenBoard(8);
     System.out.println(qb.toString());
     qb.addQueen(1,1);
+    System.out.println(qb.toString());
+    qb.removeQueen(1,1);
+    System.out.println(qb.toString());
+    qb.solve();
     System.out.println(qb.toString());
   }
 }
